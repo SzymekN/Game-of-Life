144f38def9ac52c72285749776c20415d9390c16
SzymekN <nowak.szymek00@gmail.com>
2022-03-09 16:41:08 +0100

    bug fixes

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 from random import randint
 import sys
+from unittest import result
 import pygame
 from pygame.sprite import Sprite
 import time as t
+import copy
 
 class Settings():
 
     def __init__(self) -> None:
-        self.to_live = [1,3,5,8]
-        self.to_ressurect = [3,5,7]
-        self.screen_width = 500
-        self.screen_height = 500
+        self.to_live = [1,2,5]
+        self.to_ressurect = [3,6]
+        self.screen_width = 1000
+        self.screen_height = 1000
 
-        self.cell_size = 5
-        self.number_of_rows = int(self.screen_width / self.cell_size)
-        self.number_of_cols = int(self.screen_height / self.cell_size)
+        self.cell_size = 4
+        self.number_of_rows = int(self.screen_height / self.cell_size)
+        self.number_of_cols = int(self.screen_width / self.cell_size)
 
 class Cell(Sprite):
 
-    heigth, width = 5, 5     
+    heigth, width = 1,1    
 
     def __init__(self):
         super().__init__()
@@ -27,13 +29,24 @@ class Cell(Sprite):
         self.rect = self.image.get_rect()
 
 
+
+def timer(func):
+
+    def wrapper(*args, **kwargs):
+        start = t.time()
+        result = func(*args, **kwargs)
+        end = t.time()
+        print(f"Function {func.__name__!r} executed in {(end-start)}s")
+        return result
+    return wrapper
+
 class GameOfLife:
 
     def __init__(self) -> None:
         pygame.init()
         
         self.settings = Settings()
-        self.cells = list()
+        self.cells = []
         self.next_generation = []
         for i in range(self.settings.number_of_rows):
             self.next_generation.append([0] * self.settings.number_of_cols)
@@ -47,72 +60,79 @@ class GameOfLife:
 
         pygame.display.set_caption("Game Of Life")
 
-
+    @timer
     def _randomize_life(self):
 
         for i in range(self.settings.number_of_rows):
             self.cells[i] = ([0] * self.settings.number_of_cols)
 
-        for i in range(500):
-            y = randint(0,99)
-            x = randint(0,99)
+        # for i in range(int(self.settings.number_of_cols * self.settings.number_of_rows / 20)):
+        for i in range(int(5000)):
+            y = randint(0,self.settings.number_of_rows-1)
+            x = randint(0,self.settings.number_of_cols-1)
             self.cells[y][x] = 1
-
 
     def _count_neighbours(self, y, x):
         neighbours = 0
 
+        xl = xr = x
+        yb = yt = y
+
         if y == 0:
-            return
+            yb = self.settings.number_of_rows
+        elif y >= self.settings.number_of_rows - 1:
+            yt = -1
 
         if x == 0:
-            return
+            xl = self.settings.number_of_cols
+        elif x >= self.settings.number_of_cols-1:
+            xr = -1
 
-        if x >= self.settings.number_of_cols - 1:
-            return
-
-        if y >= self.settings.number_of_rows - 1:
-            return
-
-        if self.cells[y-1][x-1]:
-            neighbours += 1
-        if self.cells[y-1][x]:
-            neighbours += 1
-        if self.cells[y-1][x+1]:
+        if self.cells[yb-1][x]:
             neighbours += 1
 
-        if self.cells[y][x-1]:
+        if self.cells[yb-1][xl-1]:
             neighbours += 1
-        if self.cells[y][x+1]:
+
+        if self.cells[yb-1][xr+1]:
+            neighbours += 1
+
+        if self.cells[y][xl-1]:
+            neighbours += 1
+        if self.cells[y][xr+1]:
             neighbours += 1
 
             
-        if self.cells[y+1][x-1]:
+        if self.cells[yt+1][xl-1]:
             neighbours += 1
-        if self.cells[y+1][x]:
+        if self.cells[yt+1][x]:
             neighbours += 1
-        if self.cells[y+1][x+1]:
+        if self.cells[yt+1][xr+1]:
             neighbours += 1
+        
+
 
         return neighbours
 
+    @timer
     def _calculate_next_gen(self):
+
 
         for y in range(self.settings.number_of_rows):
             for x in range(self.settings.number_of_cols):
                 neighbours = self._count_neighbours(y,x)
                 
-                if (neighbours in self.settings.to_live) and self.cells[y][x]:
+                if neighbours in self.settings.to_live and self.cells[y][x]:
                     self.next_generation[y][x] = 1
                     
-                elif neighbours in self.settings.to_ressurect:
+                elif neighbours in self.settings.to_ressurect and not self.cells[y][x]:
                     self.next_generation[y][x] = 1
                 
                 else:
                     self.next_generation[y][x] = 0
 
 
-        self.cells = self.next_generation
+        self.cells = copy.deepcopy(self.next_generation)
 
     def run_game(self):
         """Start main game"""
@@ -120,7 +140,7 @@ class GameOfLife:
         self._randomize_life()
         while True:
             # a = input("w")
-            pygame.time.Clock().tick(25)
+            pygame.time.Clock().tick(30)
             self._check_events()
             self._update_screen()
             self._calculate_next_gen()
@@ -140,29 +160,28 @@ class GameOfLife:
         if event.key == pygame.K_ESCAPE:
             sys.exit()
 
+    def _check_keyup_events(self, event):
+        """Check keydown event triggered"""
+        if event.key == pygame.K_ESCAPE:
+            sys.exit()
 
+    @timer
     def _draw_life(self):
 
-        for row in range(len(self.cells)):
-            for col in range(len(self.cells[row])):
+        s = self.settings.cell_size
+
+        for row in range(self.settings.number_of_rows):
+            for col in range(self.settings.number_of_cols):
                 if self.cells[row][col]:
-                    pygame.draw.rect(self.screen, (255,255,255), pygame.Rect(row*5,col*5,5,5))
+                    pygame.draw.rect(self.screen, (255,255,255), pygame.Rect(col*s,row*s,s,s))
                 else:
-                    pygame.draw.rect(self.screen, (0,0,0), pygame.Rect(row*5,col*5,5,5))
+                    pygame.draw.rect(self.screen, (0,0,0), pygame.Rect(col*s,row*s,s,s))
 
     def _update_screen(self):
         """Update objects on screen"""
         self._draw_life()
         pygame.display.flip()
 
-    def _generate_cells(self):
-
-        for row_number in range(len(self.life)):
-            for column_number in range(len(self.life[row_number])):
-                tile = Cell()
-                tile.rect.x = column_number * 5
-                tile.rect.y = row_number * 5
-                self.tiles.add(tile)
 
 
 
