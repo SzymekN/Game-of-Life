a2fc55b927ce6428ab5a0fc76a58b46ff9e9f546
SzymekN <nowak.szymek00@gmail.com>
2022-03-16 12:11:34 +0100

    comments

--- main.py
+++ main.py
@@ -1,36 +1,32 @@
 from random import randint
+from copy import deepcopy
 import sys
-from unittest import result
 import pygame
-from pygame.sprite import Sprite
 import time as t
-import copy
 
 class Settings():
-
+    '''Settings of the Game of Life'''
     def __init__(self) -> None:
-        self.to_live = [1,2,5]
-        self.to_ressurect = [3,6]
+
+        # living cells survive with this numbers of neighbours
+        self.to_live = [2,3]
+        # dead cells get alive with this numbers of neighbours
+        self.to_ressurect = [3]
+
+        # widnow size
         self.screen_width = 1000
         self.screen_height = 1000
 
+        # size of single cell
         self.cell_size = 4
+
+        # size of array
         self.number_of_rows = int(self.screen_height / self.cell_size)
         self.number_of_cols = int(self.screen_width / self.cell_size)
 
-class Cell(Sprite):
-
-    heigth, width = 1,1    
-
-    def __init__(self):
-        super().__init__()
-        self.image = pygame.Surface([Cell.width, Cell.heigth])
-        self.image.fill((255,255,255))
-        self.rect = self.image.get_rect()
-
-
 
 def timer(func):
+    '''Calculating running time of some functions'''
 
     def wrapper(*args, **kwargs):
         start = t.time()
@@ -38,21 +34,26 @@ def timer(func):
         end = t.time()
         print(f"Function {func.__name__!r} executed in {(end-start)}s")
         return result
+
     return wrapper
 
-class GameOfLife:
+class GameOfLife():
+    '''Main class running the game'''
 
     def __init__(self) -> None:
+
         pygame.init()
         
         self.settings = Settings()
+
+        # current generation of life
         self.cells = []
+        # next generation of life
         self.next_generation = []
+
+        # set deafult values to lists
         for i in range(self.settings.number_of_rows):
             self.next_generation.append([0] * self.settings.number_of_cols)
-
-
-        for i in range(self.settings.number_of_rows):
             self.cells.append([0] * self.settings.number_of_cols)
 
         self.screen = pygame.display.set_mode(
@@ -62,17 +63,17 @@ class GameOfLife:
 
     @timer
     def _randomize_life(self):
+        '''Randomize seed'''
 
-        for i in range(self.settings.number_of_rows):
-            self.cells[i] = ([0] * self.settings.number_of_cols)
-
-        # for i in range(int(self.settings.number_of_cols * self.settings.number_of_rows / 20)):
-        for i in range(int(5000)):
+        # set 20% of cells alive 
+        for i in range(int(self.settings.number_of_cols * self.settings.number_of_rows / 5)):
+        # for i in range(int(5000)):
             y = randint(0,self.settings.number_of_rows-1)
             x = randint(0,self.settings.number_of_cols-1)
             self.cells[y][x] = 1
 
     def _count_neighbours(self, y, x):
+        '''Count how many neighbours cell has'''
         neighbours = 0
 
         xl = xr = x
@@ -90,10 +91,8 @@ class GameOfLife:
 
         if self.cells[yb-1][x]:
             neighbours += 1
-
         if self.cells[yb-1][xl-1]:
             neighbours += 1
-
         if self.cells[yb-1][xr+1]:
             neighbours += 1
 
@@ -101,7 +100,6 @@ class GameOfLife:
             neighbours += 1
         if self.cells[y][xr+1]:
             neighbours += 1
-
             
         if self.cells[yt+1][xl-1]:
             neighbours += 1
@@ -110,13 +108,11 @@ class GameOfLife:
         if self.cells[yt+1][xr+1]:
             neighbours += 1
         
-
-
         return neighbours
 
     @timer
     def _calculate_next_gen(self):
-
+        '''Calculate which cells should be alive in next generation'''
 
         for y in range(self.settings.number_of_rows):
             for x in range(self.settings.number_of_cols):
@@ -131,16 +127,14 @@ class GameOfLife:
                 else:
                     self.next_generation[y][x] = 0
 
-
-        self.cells = copy.deepcopy(self.next_generation)
+        self.cells = deepcopy(self.next_generation)
 
     def run_game(self):
         """Start main game"""
-        self.start = t.time()
+
         self._randomize_life()
+
         while True:
-            # a = input("w")
-            pygame.time.Clock().tick(30)
             self._check_events()
             self._update_screen()
             self._calculate_next_gen()
@@ -167,13 +161,16 @@ class GameOfLife:
 
     @timer
     def _draw_life(self):
-
+        '''Draw window with life highlited'''
+        
         s = self.settings.cell_size
 
         for row in range(self.settings.number_of_rows):
             for col in range(self.settings.number_of_cols):
+
                 if self.cells[row][col]:
                     pygame.draw.rect(self.screen, (255,255,255), pygame.Rect(col*s,row*s,s,s))
+
                 else:
                     pygame.draw.rect(self.screen, (0,0,0), pygame.Rect(col*s,row*s,s,s))
 
@@ -182,9 +179,7 @@ class GameOfLife:
         self._draw_life()
         pygame.display.flip()
 
-
-
-
 if __name__ == "__main__":
+
     gof = GameOfLife()
     gof.run_game()
